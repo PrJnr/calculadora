b7319040cb2f49233872f48cbcb0c9b7eadc7ad9
Paulo Ricardo F. S. Junior <pauloricardo.sistemas@hotmail.com>
2020-08-01 15:31:51 -0300

    Componente Display + Estilização

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import './Calculator.css';
 
 import Button from '../components/Button'
+import Display from '../components/Display'
 
 export default class Calculator extends Component {
     render(){
@@ -9,6 +10,7 @@ export default class Calculator extends Component {
            
               
                 <div className="calculator">
+                    <Display value={100}/>
                     <Button label="AC"/>
                     <Button label="/"/>
                     <Button label="7"/>
