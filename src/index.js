903f7479ed096a7352b2c8d7dc29fd7ba368dc63
Paulo Ricardo F. S. Junior <pauloricardo.sistemas@hotmail.com>
2020-08-01 14:48:54 -0300

    Iniciando o projeto + Esquelo da Aplicação

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Calculator from './main/Calculator.jsx';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div>
+      <h1>Calculadora Pastel</h1>
+    <Calculator />
+    </div>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
