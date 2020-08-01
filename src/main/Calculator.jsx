a8669903d9c4c3a26eba8dbad7ae0446c4570da8
Paulo Ricardo F. S. Junior <pauloricardo.sistemas@hotmail.com>
2020-08-01 16:37:13 -0300

    Implementando a Logica utilizando setState

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -4,9 +4,19 @@ import './Calculator.css';
 import Button from '../components/Button'
 import Display from '../components/Display'
 
+const initialState = {
+    displayValue: '0',
+    clearDisplay: false,
+    operation: null,
+    values: [0,0],
+    current: 0,
+}
+
 export default class Calculator extends Component {
+    state = {...initialState}
+
     clearMemory( ) {
-        console.log('limpar')
+        this.setState({...initialState})
     }
 
     setOperation(operation) {
@@ -14,7 +24,22 @@ export default class Calculator extends Component {
     }
 
     addDigit(n) {
-        console.log(n)
+        if(n === '.' && this.state.displayValue.includes('.')) {
+            return
+        }
+
+        const clearDisplay = this.state.displayValue==='0' || this.state.clearDisplay
+        const currentValue = clearDisplay ? '' : this.state.displayValue;
+        const displayValue = currentValue + n;
+        this.setState({displayValue, clearDisplay: false})
+
+        if(n !== '.') {
+            const i = this.state.current;
+            const newValue = parseFloat(displayValue);
+            const values = [...this.state.values];
+            values[i] = newValue;
+            this.setState({values})
+        }
     }
 
 
@@ -26,7 +51,7 @@ export default class Calculator extends Component {
            
               
                 <div className="calculator">
-                    <Display value={100}  />
+                    <Display value={this.state.displayValue}  />
                     <Button label="AC" click={() => this.clearMemory()} triple />
                     <Button label="/" click={setOperation} operation />
                     <Button label="7" click={addDigit}  />
